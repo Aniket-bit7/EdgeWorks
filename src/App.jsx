c617cd18f1b2619a5dd36b6e354b726f96324a93
Aniket-bit7 <aniketpathak1073@gmail.com>
2025-09-26 23:27:45 +0530

    done with ui of tools and testimonials section

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,35 @@ import RemoveObject from "./pages/RemoveObject";
 import ReviewResume from "./pages/ReviewResume";
 import Community from "./pages/Community";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/ai" element={<Layout />}>
-          <Route index element={<Dashboard/>} />
-          <Route path="write-article" element={<WriteArticle/>} />
-          <Route path="blog-titles" element={<BlogTitles/>} />
-          <Route path="generate-images" element={<GenerateImages/>} />
-          <Route path="remove-background" element={<RemoveBackground/>} />
-          <Route path="remove-object" element={<RemoveObject/>} />
-          <Route path="review-resume" element={<ReviewResume/>} />
-          <Route path="community" element={<Community/>} />
+          <Route index element={<Dashboard />} />
+          <Route path="write-article" element={<WriteArticle />} />
+          <Route path="blog-titles" element={<BlogTitles />} />
+          <Route path="generate-images" element={<GenerateImages />} />
+          <Route path="remove-background" element={<RemoveBackground />} />
+          <Route path="remove-object" element={<RemoveObject />} />
+          <Route path="review-resume" element={<ReviewResume />} />
+          <Route path="community" element={<Community />} />
         </Route>
       </Routes>
+
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnHover
+        theme="colored"
+      />
     </div>
   );
 };
