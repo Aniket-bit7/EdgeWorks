879b5e5b96351fad2a06c5bd7aa96ce49dc5bc11
Aniket-bit7 <aniketpathak1073@gmail.com>
2025-11-05 04:14:05 +0530

    error debug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./Components/Navbar";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
-import ProtectedRoute from "./Components/ProtectedRoute";
+
+import ProtectedRoute from "./components/ProtectedRoute";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Navbar />
+    <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
