21f93477afc2d4f4fbcc87c2549d54e51ea5012d
Aniket-bit7 <aniketpathak1073@gmail.com>
2025-11-05 03:47:50 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -76,5 +76,8 @@ function verifyToken(req, res, next) {
 }
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
