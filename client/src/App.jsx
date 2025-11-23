222f852ef3246e84340d05510e511d34860d4ba6
Aniket-bit7 <aniketpathak1073@gmail.com>
2025-11-24 02:17:43 +0530

    hero section for home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,39 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import Dashboard from "./pages/Dashboard";
+import WriteArticle from "./pages/WriteArticle";
+import BlogTitles from "./pages/BlogTitles";
+import GenerateImages from "./pages/GenerateImages";
+import RemoveBackground from "./pages/RemoveBackground";
+import RemoveObject from "./pages/RemoveObject";
+import ReviewResume from "./pages/ReviewResume";
+import Community from "./pages/Community";
+import About from "./pages/About";
+import ContactUs from "./pages/ContactUs";
+import Pricing from "./pages/Pricing";
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ai" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="write-article" element={<WriteArticle />} />
+          <Route path="blog-titles" element={<BlogTitles />} />
+          <Route path="generate-images" element={<GenerateImages />} />
+          <Route path="remove-background" element={<RemoveBackground />} />
+          <Route path="remove-object" element={<RemoveObject />} />
+          <Route path="review-resume" element={<ReviewResume />} />
+          <Route path="community" element={<Community />} />
+        </Route>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contactUs" element={<ContactUs/>}/>
+        <Route path="/pricing" element={<Pricing/>}/>
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Hero = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <section className="bg-[url('https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/hero/gridBackground.png')] w-full bg-no-repeat bg-cover bg-center text-sm pb-44 font-[Poppins]">
+      <nav className="flex items-center justify-between p-4 md:px-16 lg:px-24 xl:px-32 md:py-6 w-full">
+        <Link to="/">
+          <h1 className="text-3xl font-semibold">EdgeWorks</h1>
+        </Link>
+
+        <div
+          className={`max-md:absolute max-md:top-0 max-md:left-0 max-md:h-full 
+          max-md:bg-white/50 max-md:backdrop-blur max-md:flex-col max-md:justify-center 
+          flex items-center gap-8 font-medium text-base transition-all duration-300 
+          ${menuOpen ? "max-md:w-full" : "max-md:w-0 max-md:overflow-hidden"}`}
+        >
+          <Link
+            to="/"
+            className="hover:text-gray-600"
+            onClick={() => setMenuOpen(false)}
+          >
+            Home
+          </Link>
+
+          <Link
+            to="/about"
+            className="hover:text-gray-600"
+            onClick={() => setMenuOpen(false)}
+          >
+            About
+          </Link>
+
+          <Link
+            to="/pricing"
+            className="hover:text-gray-600"
+            onClick={() => setMenuOpen(false)}
+          >
+            Pricing
+          </Link>
+          <Link to="https://github.com/Aniket-bit7/EdgeWorks">
+            Github
+          </Link>
+
+          <button
+            className="md:hidden bg-gray-800 hover:bg-black text-white p-2 rounded-md aspect-square transition"
+            onClick={() => setMenuOpen(false)}
+          >
+            ✖
+          </button>
+        </div>
+
+        <Link to="/contactUs">
+          <button className="hidden md:block bg-gray-800 hover:bg-black text-white px-6 py-3 rounded-full font-medium transition">
+            Contact Us
+          </button>
+        </Link>
+
+        <button
+          className="md:hidden bg-gray-800 hover:bg-black text-white p-2 rounded-md aspect-square transition"
+          onClick={() => setMenuOpen(true)}
+        >
+          ☰
+        </button>
+      </nav>
+
+      <div
+        className="flex items-center justify-center gap-2 border border-slate-300 hover:border-slate-400/70 
+  rounded-full mx-auto px-4 py-2 
+  mt-24 sm:mt-32 md:mt-36 lg:mt-40 
+  text-[10px] xs:text-xs sm:text-sm md:text-base 
+  max-w-[90%] sm:max-w-[70%] md:max-w-max text-center leading-tight"
+      >
+        <span>Empowering creators to do more with less effort.</span>
+      </div>
+
+      <h5 className="text-4xl md:text-7xl font-medium max-w-[850px] text-center mx-auto mt-8 px-2">
+        Create more. Think less. Let AI do the work.
+      </h5>
+
+      <p className="text-sm md:text-base mx-auto max-w-2xl text-center mt-6 px-4">
+        Unlock peak productivity with our AI-powered creative hub. From
+        expressive articles to high-quality visuals, get everything you need to
+        bring ideas to life instantly.
+      </p>
+
+      <div className="mx-auto w-full flex flex-wrap items-center justify-center gap-3 mt-4 px-2">
+        <button className="bg-slate-800 hover:bg-black text-white px-6 py-3 rounded-full font-medium transition w-max">
+          Get Started
+        </button>
+
+        <button className="flex items-center gap-2 border border-slate-300 hover:bg-slate-200/30 rounded-full px-6 py-3 w-max">
+          <span>Learn More</span>
+          <svg width="6" height="8" fill="none" viewBox="0 0 6 8">
+            <path
+              d="M1.25.5 4.75 4l-3.5 3.5"
+              stroke="#050040"
+              strokeOpacity=".4"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
