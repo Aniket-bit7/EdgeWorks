4cf4cc7c7487480b54929d7c9b2cf9ef3736ad2d
Aniket-bit7 <aniketpathak1073@gmail.com>
2025-11-30 04:52:08 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -28,6 +28,9 @@ app.use("/auth", authRoutes);
 app.use("/payment", paymentRoutes);
 
 
-app.listen(process.env.PORT, () => {
-  console.log("Server running on port", process.env.PORT);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
+
