a0ef49b68b0abbd4ae3638339f5713a92613b4ec
Aniket-bit7 <aniketpathak1073@gmail.com>
2025-11-30 06:28:09 +0530

    api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,16 +1,16 @@
 import axios from "axios";
-const BASE_URL =
-  import.meta.env.VITE_API_URL || "https://edgeworks.onrender.com";
 
 export const api = axios.create({
-  baseURL: BASE_URL.replace(/\/$/, ""), // remove trailing slash
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("accessToken");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
