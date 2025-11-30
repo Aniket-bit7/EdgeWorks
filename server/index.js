af39860bf8360c6f630f25d8d19a5dcf6194c4a0
Aniket-bit7 <aniketpathak1073@gmail.com>
2025-11-30 07:06:15 +0530

    ..

--- server/index.js
+++ server/index.js
@@ -12,9 +12,12 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:5173", 
+      "http://localhost:5173",
+      "http://localhost:5174",  
+      "http://localhost:3000",
       "https://edge-works.vercel.app"
     ],
+
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
